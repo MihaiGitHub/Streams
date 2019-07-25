e418941a957a07c5c5067b120d860991a037f8ce
unknown <mihai.sanfran@gmail.com>
2019-07-24 22:47:08 -0500

    Wire up the Google API library

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    componentDidMount(){
+        window.gapi.load('client:auth2', () => {
+            // Callback after this library has loaded
+            window.gapi.client.init({
+                clientId: 'xxxxxxxxxxxxxxxxxxxx',
+                scope: 'email'
+            });
+        });
+    }
+
     render(){
         return (
             <div>GoogleAuth</div>

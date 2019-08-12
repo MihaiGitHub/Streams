b0d8a6a0c13cf5c24c62b179646e8b50a47cb3ba
unknown <mihai.sanfran@gmail.com>
2019-08-11 22:19:11 -0500

    Create Redux form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+    renderInput(){
+        return <input />;
+    }
+
+    render(){
+        // this.props will have a lot of props due to reduxForm
+        console.log(this.props)
+
+        return (
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    }
 };
 
-export default StreamCreate;
+// This component will be passed a lot of new props now
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);

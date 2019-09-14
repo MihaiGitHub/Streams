27e289d99aa197aa37e9d569c1a208edd0e65747
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-13 17:16:17 -0700

    Change to Router to be able to use history object for programmatic navigation

---
+++ client/src/history.js
@@ -0,0 +1,4 @@
+// history package installed automatically with react-router-dom
+import { createBrowserHistory } from 'history';
+// create a history object that we can maintain and get access to anywhere inside our project
+export default createBrowserHistory();

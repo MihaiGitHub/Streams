6d7702ba4afece234eb2398c3936dc34c32b5f21
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-13 18:34:20 -0700

    Wildcard navigation for StreamEdit

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
+    console.log(props)
     return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+// Get list of streams into component
+// ownProps -> reference to props object which shows up inside this component
+const mapStateToProps = (state, ownProps) => {
+    // Get stream id from URL which is coming into this component as props.match.params.id
+    return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps)(StreamEdit);

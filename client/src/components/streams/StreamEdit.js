04d2ab020cd4fea49ef6a978aad924f63e2969ba
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-14 17:40:32 -0700

    Refactor edit stream

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount(){
         this.props.fetchStream(this.props.match.params.id);
     }
+
+    onSubmit = (formValues) => {
+        console.log(formValues)
+    };
     
     render(){
         if(!this.props.stream){
             return <div>Loading...</div>;
         }
 
-        return <div>{this.props.stream.title}</div>;
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                {/* initialValues is a special property name with reduxForm which puts these values in the textbox */}
+                <StreamForm 
+                    initialValues={{ title: 'EDIT ME', description: 'CHANGE ME TOO' }} 
+                    onSubmit={this.onSubmit} />
+            </div>
+        );
     }
 };
 
@@ -23,4 +36,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);

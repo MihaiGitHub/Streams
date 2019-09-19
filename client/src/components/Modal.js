5e575f4f09e60664c9b486e094e473d9b53c5595
MihaiGitHub <mihai.sanfran@gmail.com>
2019-09-18 22:12:59 -0700

    Hide modal onClick

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 // Modal to attach to other <body><div id="modal"> element
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
+        <div onClick={() => history.push('/') } className="ui dimmer modals visible active">
+            <div onClick={(e) => e.stopPropagation() } className="ui standard modal visible active">
                 <div className="header">Delete Stream</div>
                 <div className="content">
                     Are you sure you want to delete this stream?

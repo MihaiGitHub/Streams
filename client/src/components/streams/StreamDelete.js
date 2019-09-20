793f1a6acd9742c1d4b9d02e2e89bc87bada7687
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-19 22:15:08 -0700

    Make Modal component reusable

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = () => {
+    // Way to pass buttons JSX to Modal component
+    // React.Fragment meets one wrapper element requirement, but has no impact on DOM whatsoever
+    // Can't use a div wrapper due to Semantic UI style breaking
+    const actions = (
+        <React.Fragment>
+            <button className="ui button">Cancel</button>
+            <button className="ui button negative">Delete</button>
+        </React.Fragment>
+    );
+
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal 
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+                />
         </div>
     );
 };
